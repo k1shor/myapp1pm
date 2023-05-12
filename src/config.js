738d85e5d1982c4381455ae70d3f525f958f2936
Kishor Munikar <k1shor@hotmail.com>
2023-05-12 13:53:40 +0545

    final

--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@
 // export const USER_API = `${process.env.REACT_APP_BACKEND_URL}/user`
 // export const ORDER_API = `${process.env.REACT_APP_BACKEND_URL}/order`
 
-export const API = process.env.REACT_APP_BACKEND_URL
+export const API = "https://backend1pm.up.railway.app/"
